5874a393b2bfd15994e008fc3e5180aae0e06b34
Gabriel Fernandez <gabofer82@gmail.com>
2011-10-29 18:25:09 -0200

    Armando la presentación se app search (incompleto)

--- apps/searchs/search.py
+++ apps/searchs/search.py
@@ -4,6 +4,8 @@ from django import forms
 from searchs.models import Searchs
 
 class SearchForm(forms.ModelForm):
+    
+    search_word = forms.CharField(widget=forms.TextInput(attrs={'class':'search-box'}))
     class Meta:
         model = Searchs
         fields = ('search_word',)
