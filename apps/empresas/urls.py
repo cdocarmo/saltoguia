efb21992ea902cc13c5718a943c664e8369111e5
Carlos Do Carmo <cdocarmo@cdsoft.com.uy>
2011-11-02 19:30:31 -0200

    Se modifico una linea ya que daba error

--- apps/empresas/urls.py
+++ apps/empresas/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import url, patterns
 
 urlpatterns = patterns('',
     #url(r'^$', 'empresas.views.empresa', name='empresa'),
-    url(r'^(?P<username>[\w-]+)/$', 'empresas.views.ver_empresa', name='ver-empresa'),
     url(r'^nueva_empresa/$', 'empresas.views.crear_empresa', name='crear-empresa'),
+    url(r'^(?P<username>[\w-]+)/$', 'empresas.views.ver_empresa', name='ver-empresa'),    
     url(r'^(?P<empresa_slug>[\w-]+)/(?P<servicio_slug>[\w-]+)/$', \
         'empresas.views.empresa_servicio_detalle', name='empresa-servicio-detalle'),
     
