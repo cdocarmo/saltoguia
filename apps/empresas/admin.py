26ab902a1197b702f637bc44d99239f58f7cd001
Carlos Do Carmo <cdocarmo@cdsoft.com.uy>
2011-11-04 10:29:14 -0200

    Se agrego poder Desactivar Servicio en Admin

--- apps/empresas/admin.py
+++ apps/empresas/admin.py
@@ -7,6 +7,10 @@ def activar_Servicio(modeladmin, request, queryset):
     queryset.update(status='2')
 activar_Servicio.short_description = "Activar Servicios Seleccionados"
 
+def desactivar_Servicio(modeladmin, request, queryset):
+    queryset.update(status='3')
+desactivar_Servicio.short_description = "Desactivar Servicios Seleccionados"
+
     
 
 class EmpresaAdmin(ModelAdmin):
@@ -16,7 +20,7 @@ class EmpresaAdmin(ModelAdmin):
 class EmpresaServicioAdmin(ModelAdmin):
     list_display = ('nombre', 'empresa', 'descripcion', 'status', 'tags', 'tipo')
     search_fields = ('nombre', 'tags')
-    actions = [activar_Servicio]
+    actions = [activar_Servicio, desactivar_Servicio]
         
 site.register(Empresa, EmpresaAdmin)
 site.register(EmpresaServicio, EmpresaServicioAdmin)
