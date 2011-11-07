4b1f8a62316eb25e60f82b3f06806047a226b1d4
Gabriel Fernandez <gabofer82@gmail.com>
2011-11-07 09:09:17 -0200

    arreglados algunos detalles en un par de forms.py

--- apps/empresa/forms.py
+++ apps/empresa/forms.py
@@ -3,8 +3,6 @@ from django.utils.translation import ugettext_lazy as _, ungettext
 from empresa.models import Empresa, EmpresaServicio
 from django.utils.translation import ugettext as _
 
-#comentario al pedo que hay que borrar
-
 class EmpresaForm(forms.ModelForm):
     step = forms.IntegerField(widget=forms.HiddenInput, initial=1)
     nombre = forms.CharField(widget=forms.TextInput(attrs={'class':'input-text'}))
