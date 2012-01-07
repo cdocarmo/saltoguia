8a32a16b1bebae198dc1e7a08c9e97b68a1f908b
Carlos Do Carmo <cdocarmo@cdsoft.com.uy>
2012-01-07 17:41:08 -0200

    Funciona el autocompletado con los nombres de los Servicios, esta faltando, filtrar por los textos que ingresan en los tags, y que no se repitan

--- apps/searchs/urls.py
+++ apps/searchs/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'searchs.views.index', name='buscar'),
-    url(r'^servicios/$', 'searchs.views.result_search', name = 'demo-search'),    
+    url(r'^servicios/$', 'searchs.views.result_search', name = 'demo-search'),
+    url(r'^get_tags/$', 'searchs.views.cargo_tags_json', name="cargo-tags-json"),    
 )
--- apps/searchs/views.py
+++ apps/searchs/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.db.models import Q
 from django.contrib.auth.models import User
-import time
+from django.utils import simplejson
 
 from empresa.models import Empresa, EmpresaServicio
 
@@ -32,9 +32,10 @@ def result_search( request ):
     if request.is_ajax():
         q = request.GET.get( 'q' )
         if q is not None:  
-            results = servicios.filter( 
-                Q( nombre__icontains = q ) |
-                Q( tags__icontains = q ) ).order_by( 'nombre' )
+            results = servicios.filter(
+                                       Q( empresa__nombre__icontains = q ) |
+                                       Q( nombre__icontains = q ) |
+                                       Q( tags__icontains = q ) ).order_by( 'nombre' )
 
             template = 'resultado.html'
             data = {
@@ -42,3 +43,12 @@ def result_search( request ):
             }
             return render_to_response( template, data, 
                                        context_instance = RequestContext( request ) )
+            
+            
+def cargo_tags_json(request):
+    servicios = EmpresaServicio.objects.filter(status=EmpresaServicio.ACTIVA).values('id', 'nombre')
+
+    response=HttpResponse(mimetype="application/javascript")
+    response.write(simplejson.dumps(list(servicios)))
+
+    return response            
