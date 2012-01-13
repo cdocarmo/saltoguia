297047612a987e14387b0648186595d95bd6f48b
Carlos Do Carmo <cdocarmo@cdsoft.com.uy>
2012-01-12 22:21:34 -0200

    Se agrego border:0px en ccs

--- apps/searchs/urls.py
+++ apps/searchs/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'searchs.views.index', name='buscar'),
-    url(r'^servicios/$', 'searchs.views.result_search', name = 'demo-search'),
+    url(r'^servicios/$', 'searchs.views.result_search', name = 'new-search'),
     url(r'^get_tags/$', 'searchs.views.cargo_tags_json', name="cargo-tags-json"),    
 )
--- apps/searchs/views.py
+++ apps/searchs/views.py
@@ -33,18 +33,18 @@ def result_search( request ):
         q = request.GET.get( 'q' )
         if q is not None:  
             results = servicios.filter(
-                                       Q( empresa__nombre__icontains = q ) |
-                                       Q( nombre__icontains = q ) |
-                                       Q( tags__icontains = q ) ).order_by( 'nombre' )
+                                      Q( empresa__nombre__contains = q ) |
+                                      Q( nombre__contains = q ) |
+                                      Q( tags__contains = q ) ).order_by( 'nombre' )
 
-            template = 'resultado.html'
-            data = {
-                'results': results,
-            }
-            return render_to_response( template, data, 
-                                       context_instance = RequestContext( request ) )
-            
-            
+        template = 'resultado.html'
+        data = {
+            'results': results,
+        }
+        return render_to_response( template, data, 
+                                   context_instance = RequestContext( request ) )
+
+
 def cargo_tags_json(request):
     servicios = EmpresaServicio.objects.filter(status=EmpresaServicio.ACTIVA).values('id', 'tags')
     xColNew = []
