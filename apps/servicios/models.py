a7ab00e61b400c265f8ec6d7cb0b2543707c60aa
gabriel <gabofer82@gmail.com>
2012-08-10 10:32:42 -0300

    Los campos de antigua clase Empresa se pasaron al modelo del perfil del usuario y se eliminó el modelo Empresa. También se renombró el modelo EmpresaServicio a Servicio modificando foreign keys.

--- apps/servicios/models.py
+++ apps/servicios/models.py
@@ -8,64 +8,64 @@ from django.contrib.auth.models import User
 from utilita.thumbs import ImageWithThumbsField
 
 
-class Empresa(models.Model):
-
-
-    PENDIENTE = 1
-    ACTIVA = 2
-    NEGADA = 3
-    
-    _STATUS = (
-        (PENDIENTE, _('PENDIENTE')),
-        (ACTIVA, _('ACTIVA')),
-        (NEGADA, _('NEGADA')),
-    )
-
-    EMPRESA = 1
-    PERSONA = 2
-
-    TIPO_EMPRESA = (
-        (EMPRESA, _('EMPRESA')),
-        (PERSONA, _('PERSONA')),
-    )
-
-    nombre = models.CharField(max_length=255)
-    telefono = models.CharField(max_length=30, blank=True, null=True, verbose_name=u'tel\xe9fono')
-    celular = models.CharField(max_length=30, blank=True, null=True)
-    tipo = models.IntegerField(choices=TIPO_EMPRESA, default=1)
-    documento = models.CharField(max_length=255, blank=True, null=True)
-    mail = models.EmailField(blank=False, unique=True) 
-    domicilio = models.CharField(max_length=255)
-    slug = models.SlugField(editable=False)
-    descripcion = models.TextField(blank=True, null=True, verbose_name=u'descripci\xf3n')
-    status = models.IntegerField(choices=_STATUS, default=1)
-    fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name=u'fecha de creaci\xf3n')
-    fecha = models.DateTimeField(blank=True, null=True)
-    user = models.ForeignKey(User)
-    logo = ImageWithThumbsField(upload_to='images/logos', 
-                                sizes=((80,80),(50,50),(125,125),(200,200)),
-                                blank=True, null=True)
-    web = models.URLField(_('Web URL'), verify_exists=True, blank=True, null=True)
-       
-    def __unicode__(self):
-        return self.nombre
-
-    @models.permalink
-    def get_absolute_url(self):
-        return ("empresa", [self.slug])
-    
-    def save(self, *args, **kwargs):
-
-        queryset = self.__class__.objects.all()
-        if self.id:
-            queryset = queryset.exclude(id=self.id)
-        self.slug = unique_slugify(self.nombre, queryset, 'slug')
-        super(self.__class__, self).save(*args, **kwargs)
-        
+#class Empresa(models.Model):
+#
+#
+#    PENDIENTE = 1
+#    ACTIVA = 2
+#    NEGADA = 3
+#    
+#    _STATUS = (
+#        (PENDIENTE, _('PENDIENTE')),
+#        (ACTIVA, _('ACTIVA')),
+#        (NEGADA, _('NEGADA')),
+#    )
+#
+#    EMPRESA = 1
+#    PERSONA = 2
+#
+#    TIPO_EMPRESA = (
+#        (EMPRESA, _('EMPRESA')),
+#        (PERSONA, _('PERSONA')),
+#    )
+#
+#    nombre = models.CharField(max_length=255)
+#    telefono = models.CharField(max_length=30, blank=True, null=True, verbose_name=u'tel\xe9fono')
+#    celular = models.CharField(max_length=30, blank=True, null=True)
+#    tipo = models.IntegerField(choices=TIPO_EMPRESA, default=1)
+#    documento = models.CharField(max_length=255, blank=True, null=True)
+#    mail = models.EmailField(blank=False, unique=True) 
+#    domicilio = models.CharField(max_length=255)
+#    slug = models.SlugField(editable=False)
+#    descripcion = models.TextField(blank=True, null=True, verbose_name=u'descripci\xf3n')
+#    status = models.IntegerField(choices=_STATUS, default=1)
+#    fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name=u'fecha de creaci\xf3n')
+#    fecha = models.DateTimeField(blank=True, null=True)
+#    user = models.ForeignKey(User)
+#    logo = ImageWithThumbsField(upload_to='images/logos', 
+#                                sizes=((80,80),(50,50),(125,125),(200,200)),
+#                                blank=True, null=True)
+#    web = models.URLField(_('Web URL'), verify_exists=True, blank=True, null=True)
+#       
+#    def __unicode__(self):
+#        return self.nombre
+#
+#    @models.permalink
+#    def get_absolute_url(self):
+#        return ("empresa", [self.slug])
+#    
+#    def save(self, *args, **kwargs):
+#
+#        queryset = self.__class__.objects.all()
+#        if self.id:
+#            queryset = queryset.exclude(id=self.id)
+#        self.slug = unique_slugify(self.nombre, queryset, 'slug')
+#        super(self.__class__, self).save(*args, **kwargs)
+#        
 
             
         
-class EmpresaServicio(models.Model):
+class Servicio(models.Model):
     
 
     PENDIENTE = 1
@@ -88,7 +88,7 @@ class EmpresaServicio(models.Model):
 
     
     nombre = models.CharField(max_length=255)
-    empresa = models.ForeignKey(Empresa)
+    #empresa = models.ForeignKey(Empresa)
     descripcion = models.TextField()
     status = models.IntegerField(choices=_STATUS, default=1)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
--- apps/users/models.py
+++ apps/users/models.py
@@ -8,19 +8,66 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(User):
+    
+    
+    PENDIENTE = 1
+    ACTIVA = 2
+    NEGADA = 3
+    
+    _STATUS = (
+        (PENDIENTE, _('PENDIENTE')),
+        (ACTIVA, _('ACTIVA')),
+        (NEGADA, _('NEGADA')),
+    )
 
+    EMPRESA = 1
+    PERSONA = 2
+
+    TIPO_EMPRESA = (
+        (EMPRESA, _('EMPRESA')),
+        (PERSONA, _('PERSONA')),
+    )
+    
     date = models.DateTimeField(default=datetime.datetime.now)
     birthday = models.DateField(null=True, blank=True)    
     location = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=255, blank=True, null=True)
     validation = models.BooleanField()
     
+    #nombre = models.CharField(max_length=255) #el nombre es de user: ej name = hygies
+    # para que no quede name = Fulanito, name_empresa = hygies (usuario = empresa)
+    telefono = models.CharField(max_length=30, blank=True, null=True, verbose_name=u'tel\xe9fono')
+    celular = models.CharField(max_length=30, blank=True, null=True)
+    tipo = models.IntegerField(choices=TIPO_EMPRESA, default=1)
+    documento = models.CharField(max_length=255, blank=True, null=True)
+    mail = models.EmailField(blank=False, unique=True) 
+    domicilio = models.CharField(max_length=255)
+    slug = models.SlugField(editable=False)
+    descripcion = models.TextField(blank=True, null=True, verbose_name=u'descripci\xf3n')
+    status = models.IntegerField(choices=_STATUS, default=1)
+    fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name=u'fecha de creaci\xf3n')
+    fecha = models.DateTimeField(blank=True, null=True)
+    #user = models.ForeignKey(User)
+    logo = ImageWithThumbsField(upload_to='images/logos', 
+                                sizes=((80,80),(50,50),(125,125),(200,200)),
+                                blank=True, null=True)
+    web = models.URLField(_('Web URL'), verify_exists=True, blank=True, null=True)
     
     def __unicode__(self):
         return _("%s %s") % (self.first_name, self.last_name)
 
+    @models.permalink
+    def get_absolute_url(self):
+        return ("empresa", [self.slug]) #ver como funciona esto
     
     def save(self, *args, **kwargs):
-        pass
+
+        queryset = self.__class__.objects.all()
+        if self.id:
+            queryset = queryset.exclude(id=self.id)
+        self.slug = unique_slugify(self.nombre, queryset, 'slug')
+        super(self.__class__, self).save(*args, **kwargs)
+    #def save(self, *args, **kwargs):
+    #    pass
         
 
