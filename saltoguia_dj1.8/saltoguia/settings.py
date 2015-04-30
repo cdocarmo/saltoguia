9f4cc0cd68b73baba9016ff894e756d6f51b5981
Gabriel Fernández <gabofer82@gmail.com>
2015-04-30 12:40:11 -0300

    Trabajando en el formulario de crear usuario (junto a servicios).

--- saltoguia_dj1.8/saltoguia/settings.py
+++ saltoguia_dj1.8/saltoguia/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'apps.usuarios',
     'apps.buscador',
 )
