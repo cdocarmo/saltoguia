c7e348164865adec69ab8b5275f10b939700da26
Carlos Do Carmo <carlos@MacBook-Air-de-Carlos.local>
2015-05-02 10:33:07 -0300

    Ahora si la actualizacion

--- saltoguia_dj1.8/saltoguia/settings_dev.py
+++ saltoguia_dj1.8/saltoguia/settings_dev.py
@@ -7,11 +7,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'saltoguia',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
